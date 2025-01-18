f556ac05b99d425ec40f8f037c4995b551beadce
CareyYuDa <2424157164@qq.com>
2025-01-18 21:44:37 +0800

    方块移动以及视错觉的补充

---
+++ Assets/Scripts/Block/BlockMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMove : MonoBehaviour
+{
+    public bool x;
+    public bool z;
+    public bool rotatex;
+    public bool rotatey;
+    public bool rotatez;
+}
