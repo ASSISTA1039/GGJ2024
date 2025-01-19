1672503fa4e5e17bc72bd0cad4dbdfde2e33c920
laest <nollaa@126.com>
2025-01-19 14:27:49 +0800

    DeathUI修改

--- Assets/Scripts/UI/Windows/DeathUI.cs
+++ Assets/Scripts/UI/Windows/DeathUI.cs
@@ -9,9 +9,23 @@ using TMPro;
 
 public class DeathUI : UIBase
 {
+    private string[] objectsToDestroy = { "Player(Clone)", "FuncBlocks(Clone)", "NoFuncBlocks(Clone)" };//TODO：此处应写需要再切换时候删除的元素
     public override void OnDisplay(object args)
     {
         //RegisterTrigger("Mode1").onClick = OnMusicUp;
+        foreach (string objName in objectsToDestroy)
+        {
+            GameObject obj = GameObject.Find(objName);
+            if (obj != null)
+            {
+                Destroy(obj); // 销毁物体
+                Debug.Log(objName + " destroyed.");
+            }
+            else
+            {
+                Debug.LogWarning(objName + " not found.");
+            }
+        }
         RegisterTrigger("DeathButton").onClick = OnDeath;
         AudioManager.Instance.StopBGM();
 
